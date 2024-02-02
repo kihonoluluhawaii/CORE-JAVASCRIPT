8ae4376c4f424bb544fdb0a050b9e05628d9bced
kiho <kihonoluluhawaii@gmail.com>
2024-02-02 22:07:48 +0900

    learn dom
    -homework

---
+++ client/chapter/dom/00.homework.js
@@ -0,0 +1,25 @@
+//querySelector
+
+const idField = document.querySelector('#idField');
+const submit = document.querySelector('.btn');
+//evnetListener
+function handleCheckId() {
+  if (this.value === 'hello') {
+    console.log('success');
+    idField.classList.remove('is-active');
+  } else {
+    console.log('error!');
+    idField.classList.add('is-active');
+  }
+}
+function handleSubmit(e) {
+  e.preventDefault();
+  console.log('제출!!');
+}
+
+idField.addEventListener('input', handleCheckId);
+// idField.addEventListener('input', handleCheckId);
+submit.addEventListener('click', handleSubmit);
+//classList
+
+//preventDefault

372df5dbc5607e9af26d56da14a24d1561da5678
kiho <kihonoluluhawaii@gmail.com>
2024-02-19 14:32:14 +0900

    learn. localStorage
    -getStorage
    -setStorage
    -deleteStorage

--- client/main.js
+++ client/main.js
@@ -1,5 +1,36 @@
-const a = 10;
+import {
+  setStorage,
+  getStorage,
+  deleteStorage,
+  getNode,
+  clearContents,
+} from './lib/index.js';
 
-console.log();
+// 1. Textfield의 값을 로컬 스토리지에 저장
 
-console.log('안녕 자바스크립트!');
+const textField = getNode('#textField');
+const clear = getNode('[data-name="clear"]');
+
+const handleTextField = (e) => {
+  const value = e.currentTarget.value;
+  setStorage('text', value);
+};
+
+const handleClear = () => deleteStorage('text').then(clearContents(textField));
+
+textField.addEventListener('input', handleTextField);
+clear.addEventListener('click', handleClear);
+
+// function init() {
+//   console.log('init');
+// }
+
+// window.addEventListener('DOMContentLoaded', init);
+
+// IIFE
+(async () => {
+  const data = await getStorage('text');
+  textField.value = data;
+})();
+
+// 2.
